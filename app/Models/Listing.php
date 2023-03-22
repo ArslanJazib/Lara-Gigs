d6c2181f9c44f931ef467c8836e77f173d1a2f37
Arslan Jazib <arslanjazib_98@yahoo.com>
2023-03-23 01:53:46 +0500

    Front Theme Integration Start

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,31 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all(){
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem Ipsum'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem Ipsum'
-            ]
-        ];
-    }
-
-    public static function find($id){
-        $listings = self::all();
-
-        foreach ($listings as $key => $listing) {
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
-
+    use HasFactory;
 }
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,3 +1,7 @@
-<h1>{{ $heading }}</h1>
-<a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }}</a>
-<p>{{ $listing['description'] }}</p>
+@extends('layout')
+
+@section('content')
+    <h1>{{ $heading }}</h1>
+    <a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }}</a>
+    <p>{{ $listing['description'] }}</p>
+@endsection
