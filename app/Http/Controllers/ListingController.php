1dde8395432316ce4b853bbad3f12b2282502d35
Arslan Jazib <arslanjazib_98@yahoo.com>
2023-03-29 02:01:55 +0500

    Search Bar Fixes

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -8,13 +8,13 @@ use Illuminate\Http\Request;
 class ListingController extends Controller
 {
     // Show all listings
-    // public function index(Request $request) Dependency Injecion Approach
+    // public function index(Request $request) Dependency Injection Approach
     
     public function index(){
         
         return view('listings.index', [
             'listings' => Listing::latest()
-            ->filter(request(['tag']))
+            ->filter(request(['tag','search']))
             ->get()
         ]);
     }
--- resources/views/components/listing-tags.blade.php
+++ resources/views/components/listing-tags.blade.php
@@ -8,7 +8,7 @@
 
     @foreach ($tags as $tag)
         <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-            <a href="{{ url('/?tag=') }}/{{ $tag }}">{{ $tag }}</a>
+            <a href="{{ url('/?tag=') }}{{ $tag }}">{{ $tag }}</a>
         </li>
     @endforeach
 
